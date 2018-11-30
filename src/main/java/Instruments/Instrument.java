2f6e071e6e472058d9d2e2f98a5f4340483988b7
Jackie Farr <jacquelynfarr@hotmail.com>
2018-11-30 16:29:46 +0000

    Instrument abstract class setup

--- src/main/java/Instruments/Instrument.java
+++ src/main/java/Instruments/Instrument.java
@@ -1,4 +1,38 @@
 package Instruments;
 
 public abstract class Instrument {
+
+    private String colour;
+    private String type;
+    private String material;
+    private int sellPrice;
+    private int buyPrice;
+
+    public Instrument(String colour, String type, String material, int sellPrice, int buyPrice){
+        this.colour = colour;
+        this.type = type;
+        this.material = material;
+        this.sellPrice = sellPrice;
+        this.buyPrice = buyPrice;
+    }
+
+    public String getColour() {
+        return this.colour;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getMaterial() {
+        return this.material;
+    }
+
+    public int getSellPrice() {
+        return this.sellPrice;
+    }
+
+    public int getBuyPrice() {
+        return this.buyPrice;
+    }
 }
